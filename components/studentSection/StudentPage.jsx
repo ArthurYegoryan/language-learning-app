57dc16c20ba66c42235121268550a1e672e31b06
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2024-02-13 17:13:40 +0400

    fix(StudentPage): fix correct student page content work with burger menu

--- components/studentSection/StudentPage.jsx
+++ components/studentSection/StudentPage.jsx
@@ -2,11 +2,20 @@
 
 import "./StudentPage.css";
 import Header from "@/generalComponents/header/Header";
+import { useSelector } from "react-redux";
 
 const StudentPage = () => {
+    const { isMenuOpen } = useSelector((state) => state.menu);
+    let className = "";
+
+    if (isMenuOpen) className="s-page-content-left-margin";
+
     return (
-        <section>
+        <section className="s-page-section">
             <Header />
+            <div className={`s-page-content ${className}`}>
+                Barlus Hayer
+            </div>
         </section>
     );
 };
